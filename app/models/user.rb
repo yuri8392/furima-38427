e26d915798f583f12412fe215c3e38c37b2ee20c
yuri8392 <yuri.yamaguchi.28@gmail.com>
2022-11-02 21:45:57 +0900

    devise テーブル作成バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :birthday
+  end
+
+  
 end
