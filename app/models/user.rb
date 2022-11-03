5d74669a05936d219e463bcfc2099332ae072af8
yuri8392 <yuri.yamaguchi.28@gmail.com>
2022-11-03 21:40:50 +0900

    ユーザーバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,19 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :last_name
-    validates :first_name
-    validates :last_name_kana
-    validates :first_name_kana
     validates :birthday
   end
-
+ 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/,message:'is invalid. Include both letters and numbers' }
+ 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,message:'is invalid. Input full-width characters'} do
+    validates :last_name
+    validates :first_name
+  end
+ 
+  with_options presence: true, format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/, message: 'is invalid. Input full-width katakana characters' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
   
 end
