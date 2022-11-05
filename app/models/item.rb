3779f01fc92e79738a84c9b6767095b39aee03cd
yuri8392 <yuri.yamaguchi.28@gmail.com>
2022-11-05 17:56:19 +0900

    Itemモデルバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+  end
+
+  with_options presence: true,
+               numericality: { other_than: 1 , message: "can't be blank"}  do
+    validates :category_id
+    validates :item_status_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :delivery_date_id
+  end
 end
