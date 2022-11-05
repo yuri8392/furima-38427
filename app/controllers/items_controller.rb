543b5e3482d0b987bac6db99a73309f9cb8346b8
yuri8392 <yuri.yamaguchi.28@gmail.com>
2022-11-05 15:31:40 +0900

    ActiveStorage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
