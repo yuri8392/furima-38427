7c7fc052b3710c228e58253b0a4ca5f12dc7c7ff
yuri8392 <yuri.yamaguchi.28@gmail.com>
2022-11-06 19:13:19 +0900

    メソッド修正(create⇨new)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
