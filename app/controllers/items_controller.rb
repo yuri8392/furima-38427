ef042b792d2e9725ce573f6b31ff23342bf82c9a
yuri8392 <yuri.yamaguchi.28@gmail.com>
2022-11-10 17:57:19 +0900

    コントローラー（edit,update）修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,12 +23,11 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
+    redirect_to action: :index unless current_user.id == @item.user_id
   end
 
   def update
-    @item.update(item_params)
-    if @item.save
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
