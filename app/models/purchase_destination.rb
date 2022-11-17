9ec236ca23c452d1df8cc40b26ac615b312257c9
yuri8392 <yuri.yamaguchi.28@gmail.com>
2022-11-17 22:14:36 +0900

    商品購入機能モデル実装

---
+++ app/models/purchase_destination.rb
@@ -0,0 +1,22 @@
+class PurchaseDestination
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token
+
+  with_options presence: true do
+    validates :token
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is follows (e.g. 123-4567)' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is too short' }
+    validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input only number' }
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Destination.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address,
+                       building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
